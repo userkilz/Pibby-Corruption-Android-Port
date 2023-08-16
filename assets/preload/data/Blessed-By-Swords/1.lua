de74ff5b01f516368a95b4d9005d60fe0724b0d0
userkilz <141530435+userkilz@users.noreply.github.com>
2023-08-16 13:54:49 -0300

    Add files via upload

---
+++ assets/preload/data/Blessed-By-Swords/1.lua
@@ -0,0 +1,10 @@
+function onStepHit()
+if curStep == 496 then
+setProperty('camHUD.alpha',0)
+setProperty('camGame.alpha',0)
+end
+if curStep == 514 then
+setProperty('camHUD.alpha',1)
+setProperty('camGame.alpha',1)
+end
+end
